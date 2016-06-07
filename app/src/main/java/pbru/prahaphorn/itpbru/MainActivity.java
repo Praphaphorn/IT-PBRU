fa5f26301006d25ea74ba68e81df07b42544bddd
Supream <Supream_955@hotmail.com>
2016-06-07 16:00:35 +0700

    Delete Finish

--- app/src/main/java/pbru/prahaphorn/itpbru/MainActivity.java
+++ app/src/main/java/pbru/prahaphorn/itpbru/MainActivity.java
@@ -1,6 +1,7 @@
 package pbru.prahaphorn.itpbru;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,7 +20,21 @@ public class MainActivity extends AppCompatActivity {
         //test Add Neq\w User
         //myManager.addNewUser("123","name","sur","user","pass");
 
+        //Delete All SQLite
+        deleteAllSQLite();
     } // Maim Method
+
+    private void deleteAllSQLite() {
+
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManager.user_table, null, null);
+
+
+    }
+
+
+
 
     public void ClickSignUpMain(View view){
         startActivity(new Intent(MainActivity.this,
