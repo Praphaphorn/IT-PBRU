18b275589f876671fdaf5cf54da40007ec072611
Supream <Supream_955@hotmail.com>
2016-06-07 15:51:45 +0700

    Add Value Finish

--- app/src/main/java/pbru/prahaphorn/itpbru/MainActivity.java
+++ app/src/main/java/pbru/prahaphorn/itpbru/MainActivity.java
@@ -7,10 +7,18 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private MyManager myManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myManager = new MyManager(this);
+        //test Add Neq\w User
+        //myManager.addNewUser("123","name","sur","user","pass");
+
     } // Maim Method
 
     public void ClickSignUpMain(View view){
