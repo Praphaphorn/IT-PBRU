cd3b21881cb8e8bf8cb41fb18a0756ca2aca1ee9
Supream <Supream_955@hotmail.com>
2016-06-08 15:38:05 +0700

    8June Finish

--- app/src/main/java/pbru/prahaphorn/itpbru/CalendarActivity.java
+++ app/src/main/java/pbru/prahaphorn/itpbru/CalendarActivity.java
@@ -35,7 +35,7 @@ public class CalendarActivity extends AppCompatActivity {
     private void confirmDialog(final int day, int month, int year) {
 
 
-        String strDate = Integer.toString(day) +
+        final String strDate = Integer.toString(day) +
                 "/"  +Integer.toString(month+1)+
                 "/" + Integer.toString(year);
 
@@ -57,6 +57,7 @@ public class CalendarActivity extends AppCompatActivity {
                 Intent intent = new Intent(CalendarActivity.this,UploadAccount.class);
                 intent.putExtra("Login", getIntent().getStringArrayExtra("Login"));
                 intent.putExtra("InOut", 0);
+                intent.putExtra("Date",strDate);
 
                 startActivity(intent);
 
@@ -72,6 +73,7 @@ public class CalendarActivity extends AppCompatActivity {
                 Intent intent = new Intent(CalendarActivity.this, UploadAccount.class);
                 intent.putExtra("Login",getIntent().getStringArrayExtra("Login"));
                 intent.putExtra("InOut", 1);
+                intent.putExtra("Date",strDate);
                 startActivity(intent);
 
                 dialog.dismiss();
--- app/src/main/java/pbru/prahaphorn/itpbru/UploadAccount.java
+++ app/src/main/java/pbru/prahaphorn/itpbru/UploadAccount.java
@@ -1,17 +1,134 @@
 package pbru.prahaphorn.itpbru;
 
+import android.net.Uri;
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class UploadAccount extends AppCompatActivity {
 
+    private TextView inOutTextView, nameTextView;
+    private Spinner spinner;
+    private EditText editText;
+    private String[] loginStrings;
+    private int inOutAnInt;
+    private String[] inOutStrings = new String[]{"บัญชีบันทึก รายรับ","บัญชีบันทึก รายจ่าย"};
+    private String[] inComStrings = new String[]{"เงินเดือน","ล่วงเวลา","สอนพิเศษ"};
+    private String[] outComStrings = new String[]{"อาหาร","เดินทาง","Entertain","การศึกษา",};
+    private ArrayAdapter<String> stringArrayAdapter;
+    private String categoryString, amountString;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_account);
+
+        inOutTextView = (TextView) findViewById(R.id.textView6);
+        nameTextView = (TextView) findViewById(R.id.textView7);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        editText = (EditText) findViewById(R.id.editText7);
+
+        loginStrings = getIntent().getStringArrayExtra("Login");
+        inOutAnInt = getIntent().getIntExtra("InOut", 0);
+
+        //Show View
+        inOutTextView.setText(inOutStrings[inOutAnInt]);
+        nameTextView.setText(loginStrings[2]+" " + loginStrings[1]);
+
+        //create Spinner
+        createSpinner();
+
+    }
+
+    private void createSpinner() {
+        switch (inOutAnInt) {
+
+            case 0:
+                stringArrayAdapter = new ArrayAdapter<String>(this,
+                        android.R.layout.simple_list_item_1,inComStrings);
+                break;
+            case 1:
+                stringArrayAdapter = new ArrayAdapter<String>(this,
+                        android.R.layout.simple_list_item_1, outComStrings);
+                break;
+
+        }//switch
+        spinner.setAdapter(stringArrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                categoryString = findCategory(position);
+
+            }
+
+
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                categoryString = findCategory(0);
+
+            }
+        });
+
+
+    }//createSpinner
+
+    private String findCategory(int position) {
+
+        String strResult = null;
+        switch (inOutAnInt) {
+            case 0:
+                strResult = inComStrings[position];
+                    break;
+            case 1:
+                strResult = outComStrings[position];
+                    break;
+        }
+        return strResult;
     }
 
 
+    public void clickUpload(View view) {
+
+        amountString = editText.getText().toString().trim();
+        if (amountString.equals("")) {
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this,"มีช่องว่าง","ให้กรอกจำนวนเงินด้วยค่ะ");
+
+        } else {
+            uploadValueToServer();
+        }
+
+    }//clickUpload
+
+    private void uploadValueToServer() {
+
+        Log.d("8June","ID user ==> "+ loginStrings[0]);
+        Log.d("8June","Date  ==> "+ getIntent().getStringExtra("Data"));
+        Log.d("8June","In_Out ==> "+ inOutAnInt);
+        Log.d("8June","Category ==> "+ categoryString);
+        Log.d("8June","Amount ==> "+ amountString);
+
+
+    }
+
+
+
+    public void clickCancel(View view) {
+        finish();
+    }//clickCancel
 }
+
